eb12ee30112aedb3363372bd80de3d3f3c7316a9
Hamse Abdi Hashi <hamseah@stud.ntnu.no>
2018-11-03 18:31:51 +0100

    added some hiera data for prometheus

--- site/profile/manifests/grafana.pp
+++ site/profile/manifests/grafana.pp
@@ -3,20 +3,22 @@
 class profile::grafana {
    class { 'grafana':
      cfg => {
-     app_mode => 'production',
-     server   => {
-       http_port     => 8080,
-     },
-     database => {
-       type          => 'sqlite3',
-       host          => '127.0.0.1:3306',
-       name          => 'grafana',
-       user          => 'root',
-       password      => '',
-     },
-     users    => {
-       allow_sign_up => false,
-     },
-   },
- }
+       app_mode => 'production',
+       server   => {
+          http_port     => 8080,
+       },
+       database => {
+          type          => 'sqlite3',
+          host          => '127.0.0.1:3306',
+          name          => 'grafana',
+          user          => 'root',
+          password      => '',
+       },
+      users    => {
+        allow_sign_up => false,
+      },
+    },
+  }
+
+  include prometheus::node_exporter
 }
